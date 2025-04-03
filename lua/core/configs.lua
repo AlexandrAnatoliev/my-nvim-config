c88f28680c739d6cba7aa152f60f31afa579c123
AlexandrAnatoliev <per-1986@list.ru>
2025-04-03 18:43:56 +0300

    fix: add basic settings

--- lua/core/configs.lua
+++ lua/core/configs.lua
@@ -1,1 +1,20 @@
-print("configs")
+-- Line Numbers
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+-- Mouse
+vim.opt.mouse = "a"
+vim.opt.mousefocus = true
+
+-- Clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- Indent Settings
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+
+-- Other
+vim.opt.scrolloff = 8
+vim.opt.wrap = false
+
