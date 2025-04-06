622b05d08ff2a119112a9c5d2dc85b20dea6fded
AlexandrAnatoliev <per-1986@list.ru>
2025-04-06 17:51:01 +0300

    add lua and java lsp-servers

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,11 @@
 return {
   {
-    'neovim/nvim-lspconfig'
+    'neovim/nvim-lspconfig',
+    config = function()
+      local lspconfig = require('lspconfig')
+      lspconfig.lua_ls.setup{}
+      lspconfig.jdtls.setup {}
+    end
+     
   }
 }
